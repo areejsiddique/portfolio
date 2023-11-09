b93cbda2c8992f209068587c8ece3c157876efde
areejsiddique <125815158+areejsiddique@users.noreply.github.com>
2023-11-09 10:38:38 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings: ["Front-end Developer", "Web Developer","Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+});
